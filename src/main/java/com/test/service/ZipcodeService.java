36036482d3c53058167f9bfe1e39e633f4fb5691
Dhanunjayudu Surisetty <dhanunjayudusurisetty@DJ-Personal-Mac.local>
2022-01-25 09:10:41 -0600

    changed to jdk 8

--- src/main/java/com/test/service/ZipcodeService.java
+++ src/main/java/com/test/service/ZipcodeService.java
@@ -1,40 +1,54 @@
 package com.test.service;
 
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.io.IOUtils;
 
+import javax.net.ssl.HttpsURLConnection;
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
+import java.net.URL;
+
+import java.net.URLConnection;
+import java.nio.charset.StandardCharsets;
 import java.time.Duration;
 import java.util.*;
+import java.io.InputStreamReader;
 
 public class ZipcodeService {
 
     final String key;
     final ObjectMapper objectMapper;
+    String GET_STR = "GET";
 
     public ZipcodeService() {
         this.key = "Du6qoQiY0wRpgJQ6Va997t6jdjSgqS480VyQkWf5WJjIzauEaqrvhtNOQldHEanO";
         objectMapper = new ObjectMapper();
     }
 
-    private static final HttpClient httpClient = HttpClient.newBuilder()
-            .version(HttpClient.Version.HTTP_1_1)
-            .connectTimeout(Duration.ofSeconds(10))
-            .build();
+    public String fetchDataFromServer(final String url) throws IOException{
+        final URL u = new URL(url);
+        final HttpsURLConnection http = (HttpsURLConnection) u.openConnection();
+        http.setRequestMethod(GET_STR);
+        return fetchContent(http, 0);
+    }
+
+    public String fetchContent(final URLConnection conn, final int timeout) throws IOException {
+        conn.setAllowUserInteraction(true);
+        if(timeout != 0) {
+            conn.setConnectTimeout(timeout);
+            conn.setReadTimeout(timeout);
+        }
+        return IOUtils.toString(new InputStreamReader(conn.getInputStream(), StandardCharsets.UTF_8));
+
+    }
 
     public String pickFirstZip(String multiDistanceURL, String currentZip, String...targetZip) throws IOException, InterruptedException {
         String target = String.join(",", targetZip);
-
-        HttpResponse<String> response = getStringHttpResponse(String.format(multiDistanceURL,key, currentZip, target));
-        System.out.println("multiDistanceURL response = "+response);
-        System.out.println("multiDistanceURL response body = "+response.body());
-        JsonNode jsonNode = objectMapper.readTree(response.body());
+        String urlLink = String.format(multiDistanceURL,key, currentZip, target);
+        String response = fetchDataFromServer(urlLink);System.out.println("multiDistanceURL response = "+response);
+        JsonNode jsonNode = objectMapper.readTree(response);
         JsonNode distances = jsonNode.get("distances");
         List<String> zipcodes = new ArrayList<>();
         Iterator<String> iterator = distances.fieldNames();
@@ -43,24 +57,11 @@ public class ZipcodeService {
     }
 
     public String retrieveDistance(String singleDistanceURL, String currentZip, String targetZip) throws IOException, InterruptedException {
-
-        HttpResponse<String> response = getStringHttpResponse(String.format(singleDistanceURL,key, currentZip, targetZip));
-        System.out.println("singleDistanceURL response = "+response);
-        System.out.println("singleDistanceURL response body = "+response.body());
-        JsonNode jsonNode = objectMapper.readTree(response.body());
+        String urlLink = String.format(singleDistanceURL,key, currentZip, targetZip);
+        String response = fetchDataFromServer(urlLink);System.out.println("singleDistanceURL response = "+response);
+        JsonNode jsonNode = objectMapper.readTree(response);
         BigDecimal distances = jsonNode.get("distance").decimalValue();
 
         return distances.toString();
     }
-
-    private HttpResponse<String> getStringHttpResponse(String uri) throws IOException, InterruptedException {
-        HttpRequest request = HttpRequest.newBuilder()
-                .GET()
-                .uri(URI.create(uri))
-                .setHeader("User-Agent", "Java 11 HttpClient Bot")
-                .build();
-
-        return httpClient.send(request, HttpResponse.BodyHandlers.ofString());
-    }
-
 }
